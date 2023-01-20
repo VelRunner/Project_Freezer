1c50a3a2d2403587fc6622d41d9b7d5d094ece8c
VelRunner <chaussoy.benoit@gmail.com>
2023-01-20 12:31:06 +0100

    gestion des utilisateurs

--- main.py
+++ main.py
@@ -17,6 +17,15 @@ def verif_identifiant(id,passwd):
             id = input("rentrer votre identifiant")
             passwd = input("rentrer votre mot de passe")
             cursor.execute('INSERT INTO user_table (user_id,user_passwd) VALUES \'%' + id,passwd + '%\'')
+            os.system('sudo adduser \'%' + id + '%\'')
+            os.system('sudo passwd \'%' + passwd + '%\'')
+            print("vous être maintenant inscrit")
+        else:
+            print("au revoir")
+            exit()
+    else:
+        print("Bienvenue ",id)
+
 
 
 ###PROGRAM###
@@ -30,3 +39,5 @@ passwd_user = input("Mot de passe : ")
 
 connexion = sqlite3.connect("bdd_freezer.db")
 cursor = connexion.cursor()
+
+verif_identifiant(id_user,passwd_user)
